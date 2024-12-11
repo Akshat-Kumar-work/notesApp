633c46ef8a02cfdab3d0084db27ec7e755c21b54
Akshat <iamakku0.0.0.1@gmail.com>
2024-12-11 08:46:35 +0530

    route mapping templates staticfile

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-from typing import Union
-from fastapi import FastAPI 
 
+from fastapi import FastAPI ,Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+#initializing fastapi application
 app = FastAPI()
 
-@app.get("/")
+#StaticFile is used to serve the files like css, js, images
+#mount is used to map the url
+app.mount('/static',StaticFiles(directory="static"),name="static")
+
+#the jinjs2templates redner the html page dynamically
+templates = Jinja2Templates(directory="templates")
+
+#here html response specify that response will be in html format
+@app.get("/",response_class=HTMLResponse)
+async def read_items(request:Request):
+    return templates.TemplateResponse("index.html",{"request":request})
+
+@app.get("/message")
 def return_string():
     return {"hello":"ji"}
 
